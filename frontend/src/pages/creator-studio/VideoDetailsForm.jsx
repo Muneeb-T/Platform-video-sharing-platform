b910ae93c9b2c5aaa42f4580a331ffcd0a6d934d
Muneeb-T <muneebmnb17@gmail.com>
2022-12-04 16:27:01 +0530

    bugs fixed

--- frontend/src/pages/creator-studio/VideoDetailsForm.jsx
+++ frontend/src/pages/creator-studio/VideoDetailsForm.jsx
@@ -1,5 +1,5 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React, { useEffect, useState,useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as Yup from 'yup';
 import languages from '../../utils/languages';
 import { useSelector, useDispatch } from 'react-redux';
@@ -67,11 +67,7 @@ function VideoDetailsForm() {
     useEffect(() => {
         getThumbnailForVideo(videoUrl);
     }, []);
-<<<<<<< HEAD
-=======
-    
->>>>>>> 4f88bf718f02c73d10716252ab4c69610a2c8926
-    const playerRef = useRef(null)
+    const playerRef = useRef(null);
     const videoJsOptions = {
         autoplay: false,
         controls: true,
