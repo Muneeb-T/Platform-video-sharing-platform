4f88bf718f02c73d10716252ab4c69610a2c8926
Muneeb T <88328343+Muneeb-T@users.noreply.github.com>
2022-12-04 15:53:37 +0530

    cors origin changed

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false, }));
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: `${process.env.FRONTEND_HOST_NAME}`, credentials: true }));
 
 jwtStrategy();
 facebookAuthStrategy();
